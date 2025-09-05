df7fae2328a0d134cd0bf7a6fbc88250141515e7
Suresh Shrestha <163701891+Suresh-Shrestha1@users.noreply.github.com>
2025-09-05 18:40:03 +0545

    Add files via upload

---
+++ Chapter wise/6.User Defined Function/no_arguments_no_return.c
@@ -0,0 +1,12 @@
+//Write a program to display a welcome message using a user-defined function.
+#include <stdio.h>
+
+void displayMessage() {
+    printf("Welcome to C Programming Functions!\n");
+}
+
+int main() {
+    displayMessage();
+    return 0;
+}
+
---
+++ Chapter wise/6.User Defined Function/no_arguments_return.c
@@ -0,0 +1,13 @@
+//Write a function that returns the current year.
+#include <stdio.h>
+
+// Function Definition
+int getYear() {
+    return 2025;
+}
+
+int main() {
+    printf("Current Year: %d\n", getYear()); // Function Call
+    return 0;
+}
+
