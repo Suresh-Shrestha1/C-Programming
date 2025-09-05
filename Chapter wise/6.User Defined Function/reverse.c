747f6c4053a9140ddbaf62d416c6c8ac18d8e805
Suresh Shrestha <163701891+Suresh-Shrestha1@users.noreply.github.com>
2025-09-05 18:06:51 +0545

    Add files via upload

---
+++ Chapter wise/6.User Defined Function/reverse.c
@@ -0,0 +1,18 @@
+//Write a program to reverse a number using recursion.
+#include <stdio.h>
+
+int reverse(int num, int rev) {
+    if (num == 0){
+    	return rev;
+	}
+    return reverse(num / 10, rev * 10 + num % 10);
+}
+
+int main() {
+    int num;
+    printf("Enter number: ");
+    scanf("%d", &num);
+    printf("Reversed number = %d\n", reverse(num, 0));
+    return 0;
+}
+
